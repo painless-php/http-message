6584c1631396cf7f4290d910f4ec96897ab468e9
Jyri Mikkola <jyri.mikkola@pp.inet.fi>
2025-06-10 14:24:34 +0300

    Add header

---
+++ src/Header.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace PainlessPHP\Http\Message;
+
+class Header
+{
+    public function __construct(
+        private string $name,
+        private string|array $values
+    )
+    {
+    }
+
+    public function __toString()
+    {
+        return "$this->name:{$this->getValue()}";
+    }
+
+    /**
+     * Get the name of the header
+     *
+     */
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the value as a singular string without parsing commas
+     *
+     */
+    public function getValue() : string
+    {
+        return is_array($this->values)
+            ? implode(', ', $this->values)
+            : $this->values;
+    }
+
+    /**
+     * Returns array of string values for this header
+     *
+     * @return array<string>
+     *
+     */
+    public function getValues() : array
+    {
+        return $this->values;
+    }
+}
---
+++ test/Unit/HeaderTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Test\Unit;
+
+use PainlessPHP\Http\Message\Header;
+use PHPUnit\Framework\TestCase;
+
+class HeaderTest extends TestCase
+{
+    public function testGetValueReturnsUnparsedValueString()
+    {
+        $header = new Header('name', ['foo', 'bar', 'baz']);
+        $this->assertSame('foo, bar, baz', $header->getValue());
+    }
+
+    public function testGetValuesReturnsArrayOfValues()
+    {
+        $values = ['foo', 'bar', 'baz'];
+        $header = new Header('name', $values);
+        $this->assertSame($values, $header->getValues());
+    }
+
+    public function testToStringReturnsCommaDelimitedHeaderString()
+    {
+        $header = new Header('name', ['foo', 'bar', 'baz']);
+        $this->assertSame('name:foo, bar, baz', $header->__toString()); }
+}
